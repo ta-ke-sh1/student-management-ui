b4e21b58a09981cf782f58c252edd51aacbd7914
takesh1 <85833065+ta-ke-sh1@users.noreply.github.com>
2024-04-11 22:51:07 +0700

    fetch coursework lecturer

--- src/layouts/course/coursework/courseworkLecturer.js
+++ src/layouts/course/coursework/courseworkLecturer.js
@@ -1,51 +1,63 @@
+import { Button } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function CourseworkLecturer(props) {
-    const [assignments, setAssignments] = useState([]);
-    const [submissions, setSubmissions] = useState([]);
-
-    useEffect(() => {
-        fetchCourseworks();
-    }, []);
-
-    function fetchCourseworks() {
-        try {
-            axios
-                .get(
-                    process.env.REACT_APP_HOST_URL +
-                        "/course/courseworks?id=" +
-                        props.course.id
-                )
-                .then((res) => {
-                    if (res.data.status) {
-                        setAssignments(res.data.data);
-                        fetchSubmissions(res.data.data);
-                    } else {
-                        props.sendToast("error", res.data.data);
-                    }
-                });
-        } catch (e) {
-            props.sendToast("error", e.toString());
-        }
-    }
-
-    const fetchSubmissions = (assignments) => {
-        assignments.forEach((assignment) => {
-            fetchAssignments(assignment.id);
-        });
-    };
+    console.log(props);
+    const [hasFetch, setHasFetch] = useState(false);
+    const [submissions, setSubmission] = useState([]);
 
     function fetchAssignments(id) {
         axios
-            .get(process.env.REACT_APP_HOST_URL + "/submission")
+            .get(process.env.REACT_APP_HOST_URL + "/submission/course", {
+                params: {
+                    id: id,
+                },
+            })
             .then((res) => {
-                if (res.status) {
+                console.log(res.data);
+                if (res.data.status) {
+                    setSubmission(res.data.data);
+                    setHasFetch(true);
                 } else {
                     props.sendToast("error", "Failed to fetch assignments");
                 }
             });
     }
 
-    return <>{assignments.map((assignment, index) => {})}</>;
+    return (
+        <>
+            <div
+                style={{
+                    borderBottom: "1px solid black",
+                    marginBottom: "20px",
+                    paddingBottom: "10px",
+                }}>
+                {props.course.assignments ? (
+                    props.course.assignments.map((assignment) => {
+                        return (
+                            <Button
+                                variant="outlined"
+                                onClick={() => {
+                                    fetchAssignments(assignment.id);
+                                }}>
+                                {assignment.id}
+                            </Button>
+                        );
+                    })
+                ) : (
+                    <></>
+                )}
+            </div>
+            <div>
+                {hasFetch && submissions.length > 0 ? (
+                    submissions.map((submission) => {
+                        return <>{submission.id}</>;
+                    })
+                ) : (
+                    <></>
+                )}
+            </div>
+        </>
+    );
 }
